3f024a70e6daf937753094629f1361230b265fb2
benitoals <benitoals@gmail.com>
2020-03-22 07:47:19 -0300

    Initial Commit

---
+++ Dice_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice_SwiftUI
+//
+//  Created by Benito A L Silva on 22/03/20.
+//  Copyright © 2020 Benito A L Silva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
